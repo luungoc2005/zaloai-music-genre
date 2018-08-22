cf95601066f986148a3bc1d2a3cc63ea85a27477
luungoc2005 <luungoc2005@gmail.com>
2018-08-22 15:00:35 +0700

    remove padding

--- 2_img_preprocess.py
+++ 2_img_preprocess.py
@@ -42,4 +42,4 @@ else:
         librosa.display.specshow(librosa.amplitude_to_db(D, ref=np.max),
             sr=22050, y_axis='mel', x_axis='time')
         plt.axis('off')
-        plt.savefig(out_file, bbox_inches='tight')
+        plt.savefig(out_file, bbox_inches='tight', pad_inches=0)
