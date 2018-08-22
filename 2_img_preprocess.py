0664e693b4798454cff8502d31daebdb6ed4c859
luungoc2005 <luungoc2005@gmail.com>
2018-08-22 15:07:30 +0700

    add a bit of space

--- 2_img_preprocess.py
+++ 2_img_preprocess.py
@@ -42,5 +42,5 @@ else:
         librosa.display.specshow(librosa.amplitude_to_db(D, ref=np.max),
             sr=22050, y_axis='mel', x_axis='time')
         plt.axis('off')
-        plt.subplots_adjust(left=0, right=0, top=0, bottom=0)
+        plt.subplots_adjust(left=0.1, right=0.2, top=0.2, bottom=0.1)
         plt.savefig(out_file, bbox_inches='tight', pad_inches=0)
