dde3096952d5e082a024517e8f52499a991d5042
luungoc2005 <luungoc2005@gmail.com>
2018-08-23 09:33:27 +0700

    add multiprocessing for images

--- 2_img_preprocess.py
+++ 2_img_preprocess.py
@@ -6,6 +6,7 @@ import librosa.display
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
+import multiprocessing as mp
 from tqdm import tqdm
 from os import path, listdir
 from utils import removeExt, toNp
@@ -18,31 +19,40 @@ args = parser.parse_args()
 
 root = args.input_dir
 output = args.output_dir
+
+def processImgFile(filename):
+    if removeExt(filename) == '':
+        return
+
+    full_path = path.join(root, filename)
+    out_file = path.join(output, removeExt(filename) + '.jpg')
+
+    if full_path[-3:] == 'npz':
+        y = np.load(full_path)['arr']
+    else:
+        y = toNp(full_path)
+    
+    if path.exists(out_file):
+        return
+    
+    D = np.abs(librosa.stft(y, n_fft=2048, hop_length=512))
+
+    librosa.display.specshow(librosa.amplitude_to_db(D, ref=np.max),
+        sr=22050, y_axis='mel', x_axis='time')
+    plt.axis('off')
+    plt.axes([0., 0., 1., 1.], frameon=False, xticks=[], yticks=[])
+    plt.savefig(out_file, bbox_inches='tight', pad_inches=0)
+    plt.close()
+
 if not path.exists(root) or not path.isdir(root):
     print('Invalid input directory')
 elif not path.exists(output) or not path.isdir(output):
     print('Invallid output directory')
 else:
-    for filename in tqdm(listdir(root)):
-        if removeExt(filename) == '':
-            continue
+    all_files = listdir(root)
+    with mp.Pool(processes=7) as p:
+        total = len(all_files)
+        with tqdm(total=total) as pbar:
+            for i, _ in tqdm(enumerate(p.imap_unordered(processImgFile, all_files))):
+                pbar.update()
 
-        full_path = path.join(root, filename)
-        out_file = path.join(output, removeExt(filename) + '.jpg')
-
-        if full_path[-3:] == 'npz':
-            y = np.load(full_path)['arr']
-        else:
-            y = toNp(full_path)
-        
-        if path.exists(out_file):
-            continue
-        
-        D = np.abs(librosa.stft(y, n_fft=2048, hop_length=512))
-
-        librosa.display.specshow(librosa.amplitude_to_db(D, ref=np.max),
-            sr=22050, y_axis='mel', x_axis='time')
-        plt.axis('off')
-        plt.axes([0., 0., 1., 1.], frameon=False, xticks=[], yticks=[])
-        plt.savefig(out_file, bbox_inches='tight', pad_inches=0)
-        plt.close()
